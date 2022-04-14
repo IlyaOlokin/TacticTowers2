949713af0a1b818123cd85a287f68c0e4c5f7631
IlyaOlokin <84718699+IlyaOlokin@users.noreply.github.com>
2022-04-14 18:53:40 +0500

    minor fixes

--- TacticTowers/Assets/Prefabs/Upgrades/AttackSpeed.cs
+++ TacticTowers/Assets/Prefabs/Upgrades/AttackSpeed.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShootDelay : Upgrade
+public class AttackSpeed : Upgrade
 {
     [SerializeField] private float bonus;
     
