e50e66b993085cc47f32ca9cf17c8b6506d92c11
IlyaOlokin <84718699+IlyaOlokin@users.noreply.github.com>
2022-05-03 15:00:26 +0500

    Railgun base

---
+++ TacticTowers/Assets/Scripts/Towers/SpecialTowers/Railgun.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Railgun : Tower
+{
+    [SerializeField] private GameObject bullet;
+    
+    
+    void Update()
+    {
+        base.Update();
+    }
+    
+    protected override void Shoot(GameObject enemy)
+    {
+        if (enemy == null) return;
+        
+        LootAtTarget(enemy);
+        
+        if (shootDelayTimer <= 0)
+        {
+            
+            RaycastHit2D[] hits;
+            hits = Physics2D.RaycastAll(transform.position, transform.up, Mathf.Infinity);
+            var newRail = Instantiate(bullet, transform.position, Quaternion.identity);
+            newRail.GetComponent<LineRenderer>().SetPosition(0, transform.position);
+            newRail.GetComponent<LineRenderer>().SetPosition(1, transform.position + transform.up * 50);
+            float multiplier = 1f;
+            for (int i = 0; i < hits.Length; i++)
+            {
+                RaycastHit2D hit = hits[i];
+                Enemy newEnemy = hit.transform.GetComponent<Enemy>();
+
+                if (newEnemy)
+                {
+                    newEnemy.TakeDamage(GetDmg() * multiplier);
+                    multiplier -= 0.1f;
+                }
+            }
+
+            shootDelayTimer = 1f / GetAttackSpeed();
+        }
+    }
+}
