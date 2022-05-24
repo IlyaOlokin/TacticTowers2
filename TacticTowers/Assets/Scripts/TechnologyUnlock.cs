651b3a4c73daebe2da6ee3f3e9debc65913fb5cd
IlyaOlokin <84718699+IlyaOlokin@users.noreply.github.com>
2022-05-24 15:27:05 +0500

    Unlock towers

---
+++ TacticTowers/Assets/Scripts/TechnologyUnlock.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TechnologyUnlock : MonoBehaviour
+{
+    [SerializeField] private GameObject unlockFill;
+    [SerializeField] private int price;
+    [SerializeField] private Text priceText;
+    [SerializeField] private GameObject button;
+    [SerializeField] private UnlockableTowers unlockableTower;
+    private bool isUnlocked;
+
+    enum UnlockableTowers
+    {
+        Frostgun,
+        Flamethrower,
+        Railgun,
+        Tesla
+    }
+
+    private void Start()
+    {
+        LoadValues();
+        
+        SetUpgradeVisuals(isUnlocked);
+        UpdateTexts();
+    }
+
+    public void Unlock()
+    {
+        if (isUnlocked) return;
+        if (!HaveEnoughMoney()) return;
+        
+        Credits.TakeCredits(price);
+        isUnlocked = true;
+        switch (unlockableTower)
+        {
+            case UnlockableTowers.Frostgun:
+                GlobalUpgrades.IsFrostGunUnlocked = true;
+                PlayerPrefs.SetString("isFrostGunUnlocked", isUnlocked.ToString());
+                break;
+            case UnlockableTowers.Flamethrower :
+                GlobalUpgrades.IsFlamethrowerUnlocked = true;
+                PlayerPrefs.SetString("isFlamethrowerUnlocked", isUnlocked.ToString());
+                break;
+            case UnlockableTowers.Railgun :
+                GlobalUpgrades.IsRailgunUnlocked = true;
+                PlayerPrefs.SetString("isRailgunUnlocked", isUnlocked.ToString());
+                break;
+            case UnlockableTowers.Tesla :
+                GlobalUpgrades.IsTeslaUnlocked = true;
+                PlayerPrefs.SetString("isTeslaUnlocked", isUnlocked.ToString());
+                break;
+        }
+        
+        SetUpgradeVisuals(isUnlocked);
+    }
+    
+    private void UpdateTexts()
+    {
+        priceText.text = isUnlocked ? "Unlocked!" : price.ToString();
+    }
+
+    private void SetUpgradeVisuals(bool unlocked)
+    {
+        UpdateTexts();
+        unlockFill.SetActive(unlocked);
+        
+    }
+    
+    private bool HaveEnoughMoney()
+    {
+        return Credits.credits >= price;
+    }
+    
+    private void LoadValues()
+    {
+        switch (unlockableTower)
+        {
+            case UnlockableTowers.Frostgun:
+                isUnlocked = GlobalUpgrades.IsFrostGunUnlocked;
+                break;
+            case UnlockableTowers.Flamethrower :
+                isUnlocked = GlobalUpgrades.IsFlamethrowerUnlocked;
+                break;
+            case UnlockableTowers.Railgun :
+                isUnlocked = GlobalUpgrades.IsRailgunUnlocked;
+                break;
+            case UnlockableTowers.Tesla :
+                isUnlocked = GlobalUpgrades.IsTeslaUnlocked;
+                break;
+        }
+    }
+}
