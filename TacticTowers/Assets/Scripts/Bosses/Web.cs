984dd91549342bb796b0ff31c4a02c32b94e9729
KirillDmit <kirilldmitriev20015@gmail.com>
2022-07-24 00:38:53 +0500

    Web directon

--- TacticTowers/Assets/Scripts/Bosses/Web.cs
+++ TacticTowers/Assets/Scripts/Bosses/Web.cs
@@ -12,4 +12,11 @@ public class Web : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.up * speed;
     }
+
+    void Update()
+    {
+        Vector2 directon = rb.velocity;
+        var angle = Mathf.Atan2(directon.y, directon.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
 }
