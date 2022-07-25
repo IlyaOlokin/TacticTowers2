21cb2fcef4bbb736f0043c7e091011e2329dc767
KirillDmit <kirilldmitriev20015@gmail.com>
2022-07-26 00:23:50 +0500

    Update BossShootZone.cs

--- TacticTowers/Assets/Scripts/Bosses/BossShootZone.cs
+++ TacticTowers/Assets/Scripts/Bosses/BossShootZone.cs
@@ -18,7 +18,7 @@ public class BossShootZone : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Tower")
+        if (collision.gameObject.CompareTag("Tower"))
         {
             boss.Shoot(collision.gameObject);
         }
