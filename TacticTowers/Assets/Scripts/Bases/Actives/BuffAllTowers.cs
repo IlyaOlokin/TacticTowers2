e17a075c5b4dfb5f8034f81082388cacfbafa848
IlyaOlokin <84718699+IlyaOlokin@users.noreply.github.com>
2022-11-19 19:17:01 +0500

    4 tower buff effect

--- TacticTowers/Assets/Scripts/Bases/Actives/BuffAllTowers.cs
+++ TacticTowers/Assets/Scripts/Bases/Actives/BuffAllTowers.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,11 +12,29 @@ public class BuffAllTowers : BaseActive
     [SerializeField] private float shootDistanceMultiplier;
 
     [SerializeField] private float duration;
-    
+
+    [SerializeField] private GameObject buffEffect;
+    private GameObject[] towers;
+
+    private void Start()
+    {
+        towers = GameObject.FindGameObjectsWithTag("Tower");
+    }
+
     public override void ExecuteActiveAbility()
     {
         GlobalBaseEffects.ApplyToAllTowersTemporary(dmgMultiplier, attackSpeedMultiplier, shootAngleMultiplier,
             shootDistanceMultiplier, duration);
         GetComponent<Base>().UpdateAbilityTimer();
+        CreateVisualEffect();
+    }
+
+    private void CreateVisualEffect()
+    {
+        foreach (var tower in towers)
+        {
+            var newBuffEffect = Instantiate(buffEffect, tower.transform.position, Quaternion.identity, tower.transform);
+            Destroy(newBuffEffect, duration);
+        }
     }
 }
---
+++ TacticTowers/Assets/Scripts/Pulse.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulse : MonoBehaviour
+{
+    private Vector3 startScale;
+    private Vector3 targetScale;
+    [SerializeField] private float scaleMultiplier; 
+    private float timer; 
+    [SerializeField] private float scaleHalfLoopDuration; 
+    private float scaleSpeed; 
+    
+    void Start()
+    {
+        startScale = transform.localScale;
+        targetScale = startScale * scaleMultiplier;
+        scaleSpeed = (startScale.x * scaleMultiplier - startScale.x) / scaleHalfLoopDuration;
+    }
+
+    
+    void Update()
+    {
+        float deltaTime = Time.deltaTime;
+        timer += deltaTime;
+
+        if (timer < scaleHalfLoopDuration)
+        {
+            transform.localScale = Vector3.MoveTowards(transform.localScale, targetScale, scaleSpeed * deltaTime);
+        }
+        else if (timer > scaleHalfLoopDuration)
+        {
+            transform.localScale = Vector3.MoveTowards(transform.localScale, startScale, scaleSpeed * deltaTime);
+
+            if (timer > scaleHalfLoopDuration * 2f)
+                timer = 0;
+        }
+    }
+}
