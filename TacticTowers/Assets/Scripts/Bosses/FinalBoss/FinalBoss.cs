9d16b331016eaa59e3eca86a02ba70c73fe7edaa
IlyaOlokin <84718699+IlyaOlokin@users.noreply.github.com>
2022-11-30 21:19:17 +0500

    ChangingPosition

--- TacticTowers/Assets/Scripts/Bosses/FinalBoss/FinalBoss.cs
+++ TacticTowers/Assets/Scripts/Bosses/FinalBoss/FinalBoss.cs
@@ -1,12 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class FinalBoss : Boss
 {
     [SerializeField] private List<Enemy> bossParts;
     [SerializeField] private List<MonoBehaviour> bossFunctionality;
+    [SerializeField] private List<Threshold> thresholds;
+    [NonSerialized] public List<Transform> positions = new List<Transform>();
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        foreach (var pos in GameObject.FindGameObjectsWithTag("BossSpawnPoint"))
+        {
+            positions.Add(pos.transform);
+        }
+    }
+
     void Update()
     {
         UpdateHp();
@@ -15,6 +29,14 @@ public class FinalBoss : Boss
     protected override void UpdateHp()
     {
         hp = maxHp + GetDamageDone();
+        for (int i = 0; i < thresholds.Count; i++)
+        {
+            if (!thresholds[i].passed && hp / maxHp < thresholds[i].hpThreshold)
+            {
+                anim.SetTrigger("ChangePosition");
+                thresholds[i].passed = true;
+            }
+        }
         if (hp <= 0) Destroy(gameObject);
     }
 
@@ -31,11 +53,26 @@ public class FinalBoss : Boss
 
     public void ActivateBossFunctionality()
     {
-        foreach (var component in bossFunctionality) component.enabled = true;
+        foreach (var component in bossFunctionality)
+        {
+            if (component == null) continue;
+            component.enabled = true;
+        }
     }
     
     public void DeactivateBossFunctionality()
     {
-        foreach (var component in bossFunctionality) component.enabled = false;
+        foreach (var component in bossFunctionality)
+        {
+            if (component == null) continue;
+            component.enabled = false;
+        }
     }
+}
+
+[Serializable]
+public class Threshold
+{
+    public float hpThreshold;
+    [NonSerialized] public bool passed;
 }
