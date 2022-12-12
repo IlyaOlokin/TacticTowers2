e3b56e8fb4d82cfb10f40394a56d179411b38a75
IlyaOlokin <84718699+IlyaOlokin@users.noreply.github.com>
2022-12-12 15:08:23 +0500

    Tower fixes

--- TacticTowers/Assets/Scripts/Towers/Tower.cs
+++ TacticTowers/Assets/Scripts/Towers/Tower.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    [NonSerialized] public readonly int[] upgradePrices = {10, 25, 40, 100, 150, 250, 500, 800, 1400, 2000, 3000, 4200, 5500, 7500};
+    [NonSerialized] public readonly int[] upgradePrices = {10, 25, 40, 100, 150, 250, 500, 800, 1400, 2000, 3000, 4200, 5500, 7500, 10000};
     [NonSerialized] public int upgradeLevel = 1;
 
     [NonSerialized] public List<GameObject> enemiesToIgnore = new List<GameObject>();
