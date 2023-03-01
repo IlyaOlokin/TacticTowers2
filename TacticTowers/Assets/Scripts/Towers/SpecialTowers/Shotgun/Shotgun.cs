7520f3afce8387e951e0e25eb532e7b336c7cefe
IlyaOlokin <84718699+IlyaOlokin@users.noreply.github.com>
2023-03-01 17:30:10 +0500

    shotgun double shot upgrade

--- TacticTowers/Assets/Scripts/Towers/SpecialTowers/Shotgun/Shotgun.cs
+++ TacticTowers/Assets/Scripts/Towers/SpecialTowers/Shotgun/Shotgun.cs
@@ -17,39 +17,63 @@ public class Shotgun : Tower
 
     [Header("Double Damage Upgrade")] 
     [SerializeField] private float doubleDamageChance;
-
+    
+    
+    [NonSerialized] public bool hasDoubleShotUpgrade;
+    
+    [Header("Double Damage Upgrade")]
+    [SerializeField] private float doubleShotChance;
+    [SerializeField] private float doubleShotDelay;
+    
     private void Start() => audioSrc = GetComponent<AudioSource>();
     private new void Update() => base.Update();
 
     protected override void Shoot(GameObject enemy)
     {
         if (enemy == null) return;
-        
+
         LootAtTarget(enemy);
-        
+
         if (shootDelayTimer <= 0)
         {
-            for (var i = 1; i <= bulletCount + bonusBullets; i++)
-            {
-                var towerRot = towerCanon.transform.eulerAngles;
-                var bulletAngle = 0f;
+            int shotsCount = 1;
+            if (hasDoubleShotUpgrade && Random.Range(0f, 1f) < doubleShotChance) shotsCount = 2;
             
-                if ((bulletCount + bonusBullets) % 2 == 1)
-                    bulletAngle = i / 2 * angleBetweenBullets * (i % 2 == 0 ? 1 : -1f);
-                else
-                    bulletAngle = i / 2 * angleBetweenBullets * (i % 2 == 0 ? 1 : -1f) -
-                                  angleBetweenBullets / 2f;
+            StartCoroutine(MakeMultipleShots(shotsCount, doubleShotDelay));
 
-                var newBullet =  Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, bulletAngle + towerRot.z));
-                var bulletComponent = newBullet.GetComponent<Bullet>();
-                bulletComponent.Dmg = GetDmg() * GetLocalDamageMultiplier();
-                bulletComponent.Speed = bulletSpeed;
-                bulletComponent.enemiesToIgnore = enemiesToIgnore;
-                bulletComponent.departurePos = transform.position;
-            }
             shootDelayTimer = 1f / GetAttackSpeed();
-            
-            audioSrc.PlayOneShot(audioSrc.clip);
+        }
+    }
+
+    private void MakeShot()
+    {
+        for (var i = 1; i <= bulletCount + bonusBullets; i++)
+        {
+            var towerRot = towerCanon.transform.eulerAngles;
+            var bulletAngle = 0f;
+
+            if ((bulletCount + bonusBullets) % 2 == 1)
+                bulletAngle = i / 2 * angleBetweenBullets * (i % 2 == 0 ? 1 : -1f);
+            else
+                bulletAngle = i / 2 * angleBetweenBullets * (i % 2 == 0 ? 1 : -1f) -
+                              angleBetweenBullets / 2f;
+
+            var newBullet = Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, bulletAngle + towerRot.z));
+            var bulletComponent = newBullet.GetComponent<Bullet>();
+            bulletComponent.Dmg = GetDmg() * GetLocalDamageMultiplier();
+            bulletComponent.Speed = bulletSpeed;
+            bulletComponent.enemiesToIgnore = enemiesToIgnore;
+            bulletComponent.departurePos = transform.position;
+        }
+        audioSrc.PlayOneShot(audioSrc.clip);
+    }
+
+    private IEnumerator MakeMultipleShots(int shotsCount, float delayInSeconds)
+    {
+        for (int i = 0; i < shotsCount; i++)
+        {
+            MakeShot();
+            yield return new WaitForSeconds(delayInSeconds);
         }
     }
 
