dac363050f4407a1cf7c117485f4721721a94bc1
AndrewShvets <shvetsac@gmail.com>
2023-03-21 15:08:02 +0500

    trials

---
+++ TacticTowers/Assets/Scripts/Trial.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trial : MonoBehaviour
+{
+    public Sprite trialImage;
+    public int index;
+    [SerializeField] public string description;
+    [SerializeField] public string present;
+    
+}
